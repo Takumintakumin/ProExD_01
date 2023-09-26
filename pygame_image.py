b015e61f9defa7c136899ee95b8662fa02534b17
Takumin <c0a2214705@edu.teu.ac.jp>
2023-09-26 15:33:09 +0900

    練習２

---
+++ pygame_image.py
@@ -0,0 +1,29 @@
+import sys
+import pygame as pg
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("EX01/fig/pg_bg.jpg")
+    B3_img = pg.image.load("EX01/fig/3.png")
+    B3_img = pg.transform.flip(B3_img, True, False)
+    B3_img = pg.transform.rotozoom(B3_img, 10, 1.0)
+
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        screen.blit(B3_img, [300,200])
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
